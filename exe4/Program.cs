c5ee98a3a3b6bdcd2a8345368eb0e81b87d57f01
«Elena <leseee4ka@bk.ru>
2023-07-22 14:29:36 +0300

    exe4

---
+++ exe4/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 4
+Console.WriteLine("Введите первое число");
+string number1 = Console.ReadLine() ?? "";
+int a = int.Parse(number1);
+
+Console.WriteLine("Введите второе число");
+string number2 = Console.ReadLine() ?? "";
+int b = int.Parse(number2);
+
+Console.WriteLine("Введите третье число");
+string number3 = Console.ReadLine() ?? "";
+int c = int.Parse(number3);
+
+int max;
+
+if (a > b)
+{
+    max = a;
+}
+else
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+
+Console.Write("Максимальное число ");
+Console.WriteLine(max);
